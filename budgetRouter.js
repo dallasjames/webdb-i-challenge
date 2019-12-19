496872f096b4480b057e69b1751f6561942ffdab
Dallas James <dallasjames42@gmail.com>
2019-12-18 18:15:43 -0700

    done

---
+++ budgetRouter.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const db = require("./data/dbConfig")
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+    try {
+        res.json(await db("accounts"))
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        res.json(await db("accounts").where("id", req.params.id))
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            id: req.body.id,
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        const [id] = await db("accounts").insert(payload)
+        res.json({message: `posted: ${payload}`})
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        await db("accounts").where("id", req.params.id).update(payload)
+        res.json(await db("accounts").where("id", req.params.id).first())
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db("accounts").where({ id: req.params.id }).del()
+        res.status(204).end()
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const accountsRouter = require('./budgetRouter');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+
+server.get("/", (req, res) => {
+    res.json({ message: "welcome" })
+})
+
+server.use((err , req, res, next) => {
+    console.log(err)
+    res.status(500).json({error: "SUM TING WONG"})
+})
 
 module.exports = server;
